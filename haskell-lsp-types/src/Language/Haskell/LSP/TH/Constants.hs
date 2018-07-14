8f75fcb3290fbe30d89b9ce4e6b0d95fee370450
Rahul Muttineni <rahulmutt@gmail.com>
2018-07-14 07:55:56 +0530

    Basic refactoring

--- haskell-lsp-types/src/Language/Haskell/LSP/TH/Constants.hs
+++ haskell-lsp-types/src/Language/Haskell/LSP/TH/Constants.hs
@@ -7,11 +7,16 @@ import           Data.Aeson.TH
 
 -- | Standard options for use when generating JSON instances
 lspOptions :: Options
-lspOptions = defaultOptions { omitNothingFields = True, fieldLabelModifier = drop 1 }
+lspOptions = defaultOptions { omitNothingFields = True, fieldLabelModifier = defaultModifier }
  -- NOTE: This needs to be in a separate file because of the TH stage restriction
 
+defaultModifier :: String -> String
+defaultModifier = drop 1 . dropWhile (/= '_')
+
 customModifier :: String -> String
-customModifier "_xdata" = "data"
-customModifier "_xtype" = "type"
-customModifier xs = drop 1 xs
+customModifier xs =
+  case defaultModifier xs of
+    "xdata" -> "data"
+    "xtype" -> "type"
+    xs'     -> xs'
 
